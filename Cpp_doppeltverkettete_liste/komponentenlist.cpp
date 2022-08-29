2985dd4756285001d3e4022ff2a7b3db9d1c09ea
Thomas Pongratz <tomp.1985@gmx.de>
2022-08-29 22:32:43 +0200

    added implementierung komponentenlist

--- Cpp_doppeltverkettete_liste/komponentenlist.cpp
+++ Cpp_doppeltverkettete_liste/komponentenlist.cpp
@@ -1,3 +1,13 @@
 #include "komponentenlist.h"
 
+KomponentenElement *getElement(int pos) {
+
+}
+
 KomponentenList::~KomponentenList() {}
+
+const Komponente *at(int pos) {}
+
+int erase(int pos) {}
+
+void push_back(Komponente *k) {}
